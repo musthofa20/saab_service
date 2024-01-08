4a6b52ddaf9677c08ff4401c153f1571646cb5f4
Nur Musthofa <nur.musthofa20@gmail.com>
2024-01-09 00:42:37 +0700

    v1

--- routes.js
+++ routes.js
@@ -6,5 +6,5 @@ module.exports = function (app) {
     app.route('/v1/UpdateBebanKubik').post(ctrl.UpdateBebanKubik);
     app.route('/v1/AddPelanggan').post(ctrl.AddPelanggan);
     app.route('/v1/GetAllPelanggan').get(ctrl.GetAllPelanggan);
-    app.route('/v1/GetPelangganById').get(ctrl.GetPelangganById);
+    app.route('/v1/GetPelangganById').post(ctrl.GetPelangganById);
 }

e9ed8c7369d67d93fa7de3feb3b7cdb267ede68f
Nur Musthofa <nur.musthofa20@gmail.com>
2024-01-09 00:24:03 +0700

    v1

--- controller.js
+++ controller.js
@@ -68,7 +68,7 @@ exports.UpdateBebanKubik = async function (req, res) {
                         'content': 'Update Failed',
                         'dataRow': error
                     };
-                    res.status(200).json(Status)
+                    res.status(300).json(Status)
                 } else {
                     var Status = {
                         'code': '200',
@@ -87,4 +87,91 @@ exports.UpdateBebanKubik = async function (req, res) {
         res.status(300).json(Status)
     }
 
+}
+
+exports.AddPelanggan = async function (req, res) {
+    try {
+        con.query('INSERT INTO saab_plg(nopel, nama, telp, meterawal, alamat, create_date, update_date) ' +
+            'VALUES (?,?,?,?,?,?,?)',
+            [req.body.nopel, req.body.nama, req.body.telp,
+            req.body.meterawal, req.body.alamat, moment(new Date()).format("yyyyMMdd")
+                , moment(new Date()).format("yyyyMMdd")],
+            function (error, rows, fields) {
+                if (error) {
+                    var Status = {
+                        'code': '300',
+                        'content': 'Insert Failed',
+                        'dataRow': error
+                    };
+                    res.status(300).json(Status)
+                } else {
+                    var Status = {
+                        'code': '200',
+                        'content': 'Insert Success',
+                        'dataRow': rows
+                    };
+                    res.status(200).json(Status)
+                }
+            });
+    } catch (error) {
+        var Status = {
+            'code': '300',
+            'content': 'Update Failed',
+            'dataRow': error
+        };
+        res.status(300).json(Status)
+    }
+
+}
+
+exports.GetAllPelanggan = async function (req, res) {
+
+    con.query('SELECT * FROM saab_plg',
+        [],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error)
+            } else {
+                if (rows.length > 0) {
+                    var Status = {
+                        'code': '200',
+                        'content': 'Records Exist',
+                        'dataRow': rows
+                    };
+                    res.status(200).json(Status)
+                } else {
+                    var Status = {
+                        'code': '300',
+                        'content': 'Records Empty'
+                    };
+                    res.status(300).json(Status)
+                }
+            }
+        });
+}
+
+exports.GetPelangganById = async function (req, res) {
+
+    con.query('SELECT * FROM saab_plg WHERE id=?',
+        [req.body.id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error)
+            } else {
+                if (rows.length > 0) {
+                    var Status = {
+                        'code': '200',
+                        'content': 'Records Exist',
+                        'dataRow': rows
+                    };
+                    res.status(200).json(Status)
+                } else {
+                    var Status = {
+                        'code': '300',
+                        'content': 'Records Empty'
+                    };
+                    res.status(300).json(Status)
+                }
+            }
+        });
 }
--- routes.js
+++ routes.js
@@ -4,4 +4,7 @@ module.exports = function (app) {
     app.route('/v1/Login').post(ctrl.Login);
     app.route('/v1/BebanKubik').get(ctrl.BebanKubik);
     app.route('/v1/UpdateBebanKubik').post(ctrl.UpdateBebanKubik);
+    app.route('/v1/AddPelanggan').post(ctrl.AddPelanggan);
+    app.route('/v1/GetAllPelanggan').get(ctrl.GetAllPelanggan);
+    app.route('/v1/GetPelangganById').get(ctrl.GetPelangganById);
 }

1cdee315f33928686bf4e4feaedaebd34fe04778
Nur Musthofa <nur.musthofa20@gmail.com>
2024-01-11 02:06:45 +0700

    v1

--- controller.js
+++ controller.js
@@ -216,40 +216,43 @@ exports.UpdatePelangganById = async function (req, res) {
                         res.status(300).json(Status)
                     } else {
                         for (var data of result.rows) {
-                            count = data.outstanding
+                            if (parseFloat(data.outstanding) > 0) {
+                                var Status = {
+                                    'code': '300',
+                                    'content': 'Masih ada Transaksi belum lunas',
+                                    'dataRow': result
+                                };
+                                res.status(300).json(Status)
+                            } else {
+                                con.query('UPDATE saab_plg SET nama=?, telp=?,alamat=?, active=?, update_date=? WHERE nopel=?',
+                                    [req.body.nama, req.body.telp, req.body.alamat, req.body.active
+                                        , moment(new Date()).format("yyyyMMDD"), req.body.nopel],
+                                    function (error, rows, fields) {
+                                        if (error) {
+                                            var Status = {
+                                                'code': '300',
+                                                'content': 'Update Failed',
+                                                'dataRow': error
+                                            };
+                                            res.status(300).json(Status)
+                                        } else {
+                                            var Status = {
+                                                'code': '200',
+                                                'content': 'Update Success',
+                                                'dataRow': rows
+                                            };
+                                            res.status(200).json(Status)
+                                        }
+                                    });
+                            }
                         }
                         if (count > 0) {
-                            var Status = {
-                                'code': '300',
-                                'content': 'Masih ada Transaksi belum lunas',
-                                'dataRow': result
-                            };
-                            res.status(300).json(Status)
+
                         }
                     }
                 });
         }
 
-        con.query('UPDATE saab_plg SET nama=?, telp=?,alamat=?, active=?, update_date=? WHERE nopel=?',
-            [req.body.nama, req.body.telp, req.body.alamat, req.body.active
-                , moment(new Date()).format("yyyyMMDD"), req.body.nopel],
-            function (error, rows, fields) {
-                if (error) {
-                    var Status = {
-                        'code': '300',
-                        'content': 'Update Failed',
-                        'dataRow': error
-                    };
-                    res.status(300).json(Status)
-                } else {
-                    var Status = {
-                        'code': '200',
-                        'content': 'Update Success',
-                        'dataRow': rows
-                    };
-                    res.status(200).json(Status)
-                }
-            });
     } catch (error) {
         var Status = {
             'code': '300',

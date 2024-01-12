d67ab54e067e9c1e3e1e278c84616d76a61d5f6e
Nur Musthofa <nur.musthofa20@gmail.com>
2024-01-12 20:06:23 +0700

    v1

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 module.exports = function (app) {
     var ctrl = require('./controller');
-
     // config
     app.route('/v1/Login').post(ctrl.Login);
     app.route('/v1/BebanKubik').get(ctrl.BebanKubik);
